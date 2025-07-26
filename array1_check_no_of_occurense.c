f0eadab01f87e72f1e6ccb2e2f2cfb39971e36a9
muskan-last <muskanwagh1608@gmail.com>
2025-07-26 14:55:22 +0530

    update the program

--- array1_check_no_of_occurense.c
+++ array1_check_no_of_occurense.c
@@ -11,21 +11,17 @@ int no_of_occurense(int a[],int size , int element){
     printf("\nthe number of occurense of %d is : %d ",element,counter);
 }
 int main(){
-  int size,element;
-  printf("\nenert the size of the array : ");
-  scanf("%d",&size);
-    int *arr[]= (int*) malloc (size*sizeof(int));
-    if(arr==NULL){
-        printf("FAIL.");
-    }
+  int size=10,element;
+  int arr[10];
 
-    for(int i = 0 ; i< size ; i++){
-        printf("enter the %dth element of the array : ");
+  printf("WELCOME TO FINDING THE NUMBER OF OCCURENSE IN THE ARRAY \n\n");
+ for(int i = 0 ; i< size ; i++){
+        printf("enter the %dth element of the array : ",i+1);
         scanf("%d",&arr[i]);
     }
 
-    printf("\n\nenter the lement you want to seethe number of occurens :");
-    scaanf("%d",&element);
+    printf("\n\nenter the element you want to seethe number of occurens :");
+    scanf("%d",&element);
 
     no_of_occurense( arr, size , element);
     return 0;
